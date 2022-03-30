891a5eccd820e34afe811048c895a72fbab01e91
MikulasMascautanu <mikulas.mascautanu@gmail.com>
2022-03-30 23:58:02 +0200

    Adding node-html-parser, logging out basic info

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
+import axios from "axios";
+import { parse } from "node-html-parser";
+const BASE_URL = "https://www.vinylbazar.net/";
+
 const callBish = async () => {
-  console.log("Hello");
+  const response = await axios.get(BASE_URL + "pop-rock-cz-sk");
+
+  if (!response || !response.data) {
+    return;
+  }
+  const root = parse(response.data);
+
+  const els = root.querySelectorAll(".productBody");
+  els.forEach((element) => {
+    console.log(element.querySelector(".productTitleContent a").text);
+    console.log(
+      element.querySelector(".productTitleContent a").rawAttributes.href
+    );
+    console.log(element.querySelector(".product_price_text").text);
+  });
 };
 
 callBish();
